f5293ad2841d1a0cd632a3aaea7360e524252c14
chumpon-asaneerat <chumponsenate@gmail.com>
2019-07-31 08:02:45 +0700

    Add style-loader and css-loader.

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 import '@riotjs/hot-reload'
 import {component} from 'riot'
-import Random from './random.riot'
+
+import '../css/style.css'
+
+import Random from '../riot/random.riot'
 
 component(Random)(document.getElementById('app'), { title: 'Hi there!' })
--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,18 @@ module.exports = {
 */
 module.exports = {
     mode: 'development',
-    entry: './app/main.js',
+    entry: { 
+        main: './app/js/main.js',
+        index: './src/index.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].js'
     },
     devtool: 'inline',
+    optimization: {
+        minimize: true
+    },
     module: {
         rules: [{
             test: /\.riot$/,
@@ -33,6 +39,20 @@ module.exports = {
                 loader: 'babel-loader',
                 options: { presets: ['@babel/preset-env'] }
             }
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+                // style-loader
+                { 
+                    loader: 'style-loader'
+                },
+                // css-loader
+                {
+                    loader: 'css-loader',
+                    options: { modules: false }
+                }
+            ]
         }]
     }
 }
